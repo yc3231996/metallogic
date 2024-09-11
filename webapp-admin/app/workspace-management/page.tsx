3ca4902415c936de0f5859efbc94b1a63ed3b057
yancy yang <yc3231996@gmail.com>
2024-09-12 01:44:14 +0800

    fix text

--- webapp-admin/app/workspace-management/page.tsx
+++ webapp-admin/app/workspace-management/page.tsx
@@ -4,7 +4,7 @@ const WorkspaceManagement = () => {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Workspace管理</h1>
-      <p>暂没开放创建workspace并连接到自定义数据源，请联系管理员创建workspace和数据源</p>
+      <p>创建workspace和数据源暂没开放，请联系管理员创建workspace和数据源</p>
       {/* 在这里添加Workspace管理的具体功能 */}
     </div>
   );
